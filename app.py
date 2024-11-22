cdd13e4d08078615e1c6c3117cd76f944811cf96
dromu <alejandromun5@gmail.com>
2024-11-22 11:02:41 -0500

    optimizacion de codigo

--- app.py
+++ app.py
@@ -3,21 +3,91 @@ import os
 from streamlit_img_label import st_img_label
 from streamlit_img_label.manage import ImageManager, ImageDirManager
 from prediction import predictParasite
-import os 
-import shutil
-from coordenates import *
+from coordenates import calculate_rect_coords
 from PIL import Image, ImageDraw, ImageFont
 from io import BytesIO
+import shutil
 
-def run(img_dir, labels):
+TEMP_DIR = "temp_files"
 
-    temp_dir = "temp_files"
-    os.makedirs(temp_dir, exist_ok=True) 
+def setup_temp_dir():
+    if not os.path.exists(TEMP_DIR):
+        os.makedirs(TEMP_DIR)
 
-    st.sidebar.image("parasitechLogo.png")
-    # Eliminar configuración obsoleta
+def reset_temp_dir():
+    if os.path.exists(TEMP_DIR):
+        shutil.rmtree(TEMP_DIR)
+        os.makedirs(TEMP_DIR)
+
+def go_to_image():
+        file_index = st.session_state["files"].index(st.session_state["file"])
+        st.session_state["image_index"] = file_index
+
+def get_image_path(option, uploaded_file, img_dir, idm):
+    if option == 'Subir' and uploaded_file:
+        temp_path = os.path.join(TEMP_DIR, uploaded_file.name)
+        with open(temp_path, "wb") as temp_file:
+            temp_file.write(uploaded_file.read())
+        return temp_path
+    
+    elif option == "Galeria":
+        reset_temp_dir()
+        img_file_name = idm.get_image(st.session_state["image_index"])
+        return os.path.join(img_dir, img_file_name)
+    return "staticImages/parasitechLogo.png"
+
+def display_predictions(preview_imgs, parasite, resized_img, coord):
+    st.session_state.predecir = True
+    columns = st.columns(3)
+    img_bytes = None
+
+    for idx, (preview_img, box_coords) in enumerate(zip(preview_imgs, coord)):
+        preview_img[0].thumbnail((200, 200))
+        col = columns[idx % 3]
+        with col:
+            col.image(preview_img[0])
+            pred_class, _, probs = predictParasite(preview_img[0], parasite)
+            max_prob = round(probs.max().item() * 100, 2)
+            st.write(pred_class, f"{max_prob}%")
+
+            draw = ImageDraw.Draw(resized_img)
+            draw.rectangle(box_coords, outline="blue", width=3)
+
+            font = ImageFont.truetype("DejaVuSans.ttf", 15) if True else ImageFont.load_default()
+            x_center = ((box_coords[0] + box_coords[2]) // 2)-30
+            draw.text((x_center, box_coords[3]), pred_class, fill="black", font=font)
+            draw.text((x_center, box_coords[3] + 20), f"{max_prob}%", fill="black", font=font)
+
+    overlay_image = Image.open("staticImages/parasitechWatermark.png").resize((189, 112))
+    resized_img.paste(overlay_image, (resized_img.width - overlay_image.width, resized_img.height - overlay_image.height), overlay_image)
+
+    img_bytes = BytesIO()
+    resized_img.save(img_bytes, format="PNG")
+    img_bytes.seek(0)
+
+    st.download_button("Descargar imagen", data=img_bytes, file_name="parasiTech.png", mime="image/png")
+
+def run():
+    img_dir = "imgExamples"
+    st.sidebar.image("staticImages/parasitechLogo.png")
+    parasite = st.sidebar.radio("Especie:", ["Protozoo", "Helminto"])
+    slider_contour = st.sidebar.slider("Selección de umbral:", 0, 255, 90)
+    opcion = st.sidebar.radio("Selecciona una opción:", ["Subir", "Galeria"])
+    
+    if opcion == "Subir":
+        uploaded_file = st.sidebar.file_uploader("Selecciona una imagen", type=["png", "jpg", "jpeg"])
+    
+    elif opcion == "Galeria":
+        st.sidebar.selectbox(
+                "Files",
+                st.session_state["files"],
+                index=st.session_state["image_index"],
+                on_change=go_to_image,
+                key="file",
+            )
+        uploaded_file = None
+    
     idm = ImageDirManager(img_dir)
-
     if "files" not in st.session_state:
         st.session_state["files"] = idm.get_all_files()
         st.session_state["annotation_files"] = idm.get_exist_annotation_files()
@@ -25,158 +95,23 @@ def run(img_dir, labels):
     else:
         idm.set_all_files(st.session_state["files"])
         idm.set_annotation_files(st.session_state["annotation_files"])
+
+    img_path = get_image_path(opcion, uploaded_file, img_dir, idm)
     
+    if img_path == "staticImages/parasitechLogo.png":
+        slider_contour = 120
 
 
-    def go_to_image():
-        file_index = st.session_state["files"].index(st.session_state["file"])
-        st.session_state["image_index"] = file_index
+    im = ImageManager(img_path, slider_contour)
+    resized_img = im.resizing_img(max_height=1000, max_width=1000)
+    rects = st_img_label(resized_img, box_color="blue", rects=im.get_resized_rects())
 
-    parasite = st.sidebar.radio("Especie:", ("Protozoo", "Helminto"))
-    sliderContour = st.sidebar.slider("Selección de umbral:",0,255,90)
-
-    opciones = ['Subir', 'Galeria']
-    opcion_seleccionada = st.sidebar.radio('Selecciona una opción:', opciones)
-
-    
-    if opcion_seleccionada == 'Subir':
-        
-        uploaded_file = st.sidebar.file_uploader("Selecciona una imagen", type=["png", "jpg", "jpeg"])
-        if uploaded_file:            
-            os.makedirs(temp_dir, exist_ok=True) 
-
-            temp_path = os.path.join(temp_dir, uploaded_file.name)
-
-            with open(temp_path, "wb") as temp_file:
-                temp_file.write(uploaded_file.read())
-
-            st.write("Ruta del archivo temporal:", temp_path)
-            img_path = temp_path
-        
-        else: 
-            img_path = "parasitechLogo.png"
-
-    elif opcion_seleccionada == "Galeria":
-        
-        #Elimina la carpeta residual si existe 
-
-        if os.path.exists(temp_dir) and os.path.isdir(temp_dir):
-            shutil.rmtree(temp_dir)
-            
-
-        st.sidebar.selectbox(
-            "Files",
-            st.session_state["files"],
-            index=st.session_state["image_index"],
-            on_change=go_to_image,
-            key="file",
-        )
-        # Main content: annotate images
-        img_file_name   = idm.get_image(st.session_state["image_index"])
-        img_path        = os.path.join(img_dir, img_file_name)
-
-        
-        
-
-    im              = ImageManager(img_path, sliderContour)
-    resized_img     = im.resizing_img(max_height=1000,max_width=1000)
-    resized_rects   = im.get_resized_rects()
-    rects           = st_img_label(resized_img, box_color="blue", rects=resized_rects)
-
-
-    if "predecir" not in st.session_state:
-        st.session_state.predecir = False
-    
-
-    if st.sidebar.button("Predecir"):
-        st.session_state.predecir = True
-
-        if rects:
-
-            coord = calculate_rect_coords(rects)
-            
-            preview_imgs = im.init_annotation(rects)
-
-            # Número de columnas
-            columns = 3
-            cantidad = len(preview_imgs)
-            filas = cantidad // columns
-            if cantidad % columns != 0:
-                filas += 1
-
-            # Crear las columnas una sola vez
-            col1, col2, col3 = st.columns(columns)
-
-            # Iterar sobre las filas
-            for fila in range(filas):
-                # Asignar las imágenes a las columnas en cada fila
-                for col_idx, column in enumerate([col1, col2, col3]):
-                    # Calcular el índice de la imagen correspondiente
-                    idx_imagen = fila * columns + col_idx
-                    if idx_imagen < cantidad:
-                        with column:
-                            preview_imgs[idx_imagen][0].thumbnail((200, 200))  # Redimensionar la imagen
-                            column.image(preview_imgs[idx_imagen][0])
-                                    
-                            if st.session_state.predecir:
-                                pred_class,pred_idx,probs = predictParasite(preview_imgs[idx_imagen][0],parasite)
-                                maxPor= round(probs.max().item() * 100,2)
-                                st.write(pred_class,maxPor)
-
-                                draw = ImageDraw.Draw(resized_img)
-                                box_coords = coord[idx_imagen]
-                                x1, y1,x2,y2 = box_coords
-                                draw.rectangle(box_coords, outline="blue", width=3)
-
-                                try:
-                                    font = ImageFont.truetype("DejaVuSans.ttf", 15) # Carga una fuente (puedes cambiarla)
-                                except IOError:
-                                    font = ImageFont.load_default()
-
-                                x0 = int((x2+x1)/2)-30
-                                
-
-                                text_position = (x0,y2)  # Coordenadas (x, y)
-                                draw.text(text_position, pred_class, fill="black", font=font)
-
-                                text_position = (x0,y2+20) # Coordenadas
-                                draw.text(text_position, str(maxPor)+" %", fill="black", font=font)
-
-                                overlay_image = Image.open("parasitechResultado.png")
-                                overlay_image = overlay_image.resize((189, 112)) 
-                                overlay_width, overlay_height = overlay_image.size
-
-                                base_width, base_height = resized_img.size
-                                position = (base_width - overlay_width, base_height - overlay_height)
-
-                                resized_img.paste(overlay_image, position, overlay_image)
-
-                                # Guardar la imagen en un archivo temporal en memoria
-                                img_bytes = BytesIO()
-                                resized_img.save(img_bytes, format="PNG")
-                                img_bytes.seek(0) 
-
-            st.download_button(
-                                label="Descargar imagen",
-                                data=img_bytes,
-                                file_name="Parasitech.png",
-                                mime="image/png"
-                                )                    
-                                
-                          
-                
-
-
-
-                        
-            st.session_state.predecir = False
-
-
-            
+    if st.sidebar.button("Predecir") and rects:
+        coord = calculate_rect_coords(rects)
+        preview_imgs = im.init_annotation(rects)
+        display_predictions(preview_imgs, parasite, resized_img, coord)
 
 if __name__ == "__main__":
-    custom_labels = ["", "dog", "cat"]
-
     st.set_page_config(layout="wide", page_title="Parasitech", page_icon="🖼️")
-
-    run("img_dir", custom_labels)
+    setup_temp_dir()
+    run()
